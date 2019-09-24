cc865b9c557a3af8da89d2b2092b9d29b35298e0
ayush577 <agandhi62@gmail.com>
2019-09-24 10:46:33 +0530

    initital commit for herballife.com

---
+++ app/js/_magnificpopup.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+  "use strict";
+
+  	// magnific popup
+  $('.image-popup').magnificPopup({
+  type: 'image',
+   gallery: {
+    enabled: false,
+  }
+  });
+
+});
